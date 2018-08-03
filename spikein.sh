a2478588f9d68424b741c31f37d3a4d7f483f67a
samarmehta <sbmehta@gmail.com>
2018-08-03 19:09:31 -0400

    renamed stuff

---
+++ spikein.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+###############################################################
+### DNANEXUS BATCH PARAMETERS: CLASSIFY_KRAKEN ################
+###############################################################
+# VERSIONS:
+#     v0.1  2018-08-03 sbm    Split off parameters from earlier "generic_launch" scripts
+#
+# Assumes running from the Broad cluster with something like the following environmentis already set up:
+#    $ use Python-2.7
+#    $ source /idi/sabeti-scratch/kjsiddle/dnanexus/dx-toolkit/environment
+#    $ dx login --token [TOKEN]
+
+
+##### TARGET DATA #####
+PROJECT="project-XXXXXXXXXXXXXXXXXXXX"
+
+INPUTDIR="/analysis"
+OUTPUTDIR="/assemblies"
+
+#declare -a SUBDIRS=("l1" "l2")    # optinally specify subdirectories
+
+ACCEPTFILTER="cleaned.bam"
+REJECTFILTER="mapped|subsamp|Unmatched"
+
+##### JOB OPTIONS #####
+APPNAME="spikein"
+APPDIR="/viral-ngs/1.21.0/spikein"
+
+RESOURCEDIR="/resources"
+REFERENCEDIR="/references"
+
+OPTIONS=""
+
+source ./dna_batch_launch.sh
